8c963594b16a18c7152f0b24596dd0c3eb777346
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-05-20 18:57:41 +0530

    profile.html created and some changes in script.js and style.css file

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ darkBtn.onclick = function(){
     document.body.classList.toggle("dark-theme");
 
 
-    if(localStorage.getItem("theme")=="light"){
+    if(localStorage.getItem("theme") == "light"){
         localStorage.setItem("theme","dark");
     }else{
         localStorage.setItem("theme","light");
@@ -21,7 +21,7 @@ darkBtn.onclick = function(){
 if(localStorage.getItem("theme")=="light"){
     darkBtn.classList.remove("dark-btn-on");
     document.body.classList.remove("dark-theme");
-}else if(localStorage.getItem("theme")=="dark"){
+}else if(localStorage.getItem("theme") == "dark"){
     darkBtn.classList.add("dark-btn-on");
     document.body.classList.add("dark-theme");
 }else{
@@ -29,5 +29,3 @@ if(localStorage.getItem("theme")=="light"){
 }
 
 
-localStorage.setItem("theme","dark");
-localStorage.getItem("theme");
